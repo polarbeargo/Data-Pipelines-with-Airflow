0d114ea92d9d02373d564bc44e1d45dbb8ab739f
alice <bow1226@gmail.com>
2021-08-21 11:24:01 +0800

    feat: Implement create_redshift_cluster.py

---
+++ airflow/create_redshift_cluster.py
@@ -0,0 +1,171 @@
+import pandas as pd
+import boto3
+import json
+import configparser
+from botocore.exceptions import ClientError
+import argparse
+
+config = configparser.ConfigParser()
+config.read_file(open('dwh.cfg'))
+
+KEY                    = config.get('AWS','KEY')
+SECRET                 = config.get('AWS','SECRET')
+
+DWH_CLUSTER_TYPE       = config.get("DWH","DWH_CLUSTER_TYPE")
+DWH_NUM_NODES          = config.get("DWH","DWH_NUM_NODES")
+DWH_NODE_TYPE          = config.get("DWH","DWH_NODE_TYPE")
+
+DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
+DWH_DB                 = config.get("DWH","DWH_DB")
+DWH_DB_USER            = config.get("DWH","DWH_DB_USER")
+DWH_DB_PASSWORD        = config.get("DWH","DWH_DB_PASSWORD")
+DWH_PORT               = config.get("DWH","DWH_PORT")
+
+DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
+
+(DWH_DB_USER, DWH_DB_PASSWORD, DWH_DB)
+
+ec2 = boto3.resource('ec2',
+                       region_name="us-west-2",
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET
+                    )
+
+s3 = boto3.resource('s3',
+                       region_name="us-west-2",
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET
+                   )
+
+iam = boto3.client('iam',aws_access_key_id=KEY,
+                     aws_secret_access_key=SECRET,
+                     region_name='us-west-2'
+                  )
+
+redshift = boto3.client('redshift',
+                       region_name="us-west-2",
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET
+                       )
+
+s3bucket =  s3.Bucket("udacity-dend")
+
+s3_logs = iter(s3bucket.objects.filter(Prefix="log-data"))
+for _ in range(10): 
+    print(next(s3_logs))
+
+s3_songs = iter(s3bucket.objects.filter(Prefix="song-data"))
+for _ in range(10): 
+    print(next(s3_songs))
+    
+def create_role():
+ 
+    #1.1 Create the role, 
+    try:
+        print("1.1 Creating a new IAM Role") 
+        dwhRole = iam.create_role(
+            Path='/',
+            RoleName=DWH_IAM_ROLE_NAME,
+            Description = "Allows Redshift clusters to call AWS services on your behalf.",
+            AssumeRolePolicyDocument=json.dumps(
+                {'Statement': [{'Action': 'sts:AssumeRole',
+                'Effect': 'Allow',
+                'Principal': {'Service': 'redshift.amazonaws.com'}}],
+                'Version': '2021-06-26'})
+        )    
+    except Exception as e:
+        print(e)
+    
+    print("1.2 Attaching Policy")
+
+    iam.attach_role_policy(RoleName=DWH_IAM_ROLE_NAME,
+                       PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+                      )['ResponseMetadata']['HTTPStatusCode']
+
+def create_cluster(roleArn):
+    try:
+        response = redshift.create_cluster(        
+        #HW
+        ClusterType=DWH_CLUSTER_TYPE,
+        NodeType=DWH_NODE_TYPE,
+        NumberOfNodes=int(DWH_NUM_NODES),
+
+        #Identifiers & Credentials
+        DBName=DWH_DB,
+        ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+        MasterUsername=DWH_DB_USER,
+        MasterUserPassword=DWH_DB_PASSWORD,
+        
+        #Roles (for s3 access)
+        IamRoles=[roleArn]  
+        )
+    except Exception as e:
+        print(e)
+
+def prettyRedshiftProps(props):
+    pd.set_option('display.max_colwidth', -1)
+    keysToShow = ["ClusterIdentifier", "NodeType", "ClusterStatus", "MasterUsername", "DBName", "Endpoint", "NumberOfNodes", 'VpcId']
+    x = [(k, v) for k,v in props.items() if k in keysToShow]
+    return pd.DataFrame(data=x, columns=["Key", "Value"])
+
+def decribe_cluster_status():
+    myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
+    prettyRedshiftProps(myClusterProps)
+    DWH_ENDPOINT = myClusterProps['Endpoint']['Address']
+    DWH_ROLE_ARN = myClusterProps['IamRoles'][0]['IamRoleArn']
+    print("DWH_ENDPOINT :: ", DWH_ENDPOINT)
+    print("DWH_ROLE_ARN :: ", DWH_ROLE_ARN)
+    
+    try:
+        vpc = ec2.Vpc(id=myClusterProps['VpcId'])
+        defaultSg = list(vpc.security_groups.all())[0]
+        print(defaultSg)
+        defaultSg.authorize_ingress(
+            GroupName=defaultSg.group_name,
+            CidrIp='0.0.0.0/0',
+            IpProtocol='TCP',
+            FromPort=int(DWH_PORT),
+            ToPort=int(DWH_PORT)
+        )
+    except Exception as e:
+        print(e)
+        logging.warning(e)
+            
+def verify_connect():    
+    conn_string="postgresql://{}:{}@{}:{}/{}".format(DWH_DB_USER, DWH_DB_PASSWORD, DWH_ENDPOINT, DWH_PORT,DWH_DB)
+    print(conn_string)
+ 
+def delete_cluster():
+    redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
+    iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+    iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
+    
+def main(args):
+    """ Main function """
+    pd.DataFrame({"Param":
+                  ["DWH_CLUSTER_TYPE", "DWH_NUM_NODES", "DWH_NODE_TYPE", "DWH_CLUSTER_IDENTIFIER", "DWH_DB", "DWH_DB_USER", "DWH_DB_PASSWORD", "DWH_PORT",
+                   "DWH_IAM_ROLE_NAME"],"Value":
+                  [DWH_CLUSTER_TYPE, DWH_NUM_NODES, DWH_NODE_TYPE, DWH_CLUSTER_IDENTIFIER, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, DWH_PORT,DWH_IAM_ROLE_NAME]
+                 })
+    
+    if args.delete:
+       delete_cluster()
+       decribe_cluster_status()
+       return
+    
+    create_role()
+    print("1.3 Get the IAM role ARN")
+    roleArn = iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
+    print(roleArn)
+    create_cluster(roleArn)
+    decribe_cluster_status()
+    print("Finished Deploy Redshift cluster")
+    
+if __name__ == '__main__':
+    """ Set logging level and cli arguments """
+#     logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--delete', dest='delete', default=False, action='store_true')
+    parser.add_argument('--query_file', dest='query_file', default=None)
+    args = parser.parse_args()
+    main(args)
